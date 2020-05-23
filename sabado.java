85b9fc36a5164ded0c8d6a73d7d0f87a5f31416c
emprendendor <andres.escobar.godella@gmail.com>
2020-05-23 13:28:22 +0200

    añado una  pagina jsp

--- sabado.java
+++ sabado.java
@@ -64,3 +64,15 @@ public class MyServlet extends HttpServlet {
                 "</html>");
     }
 }
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>  Error JSP </title>
+</head>
+<body>
+<h1>  Error jsp</h1>
+</body>
+</html>
