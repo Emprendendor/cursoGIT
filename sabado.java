12b546f86737509118cae9106dc28d0523304e0b
emprendendor <andres.escobar.godella@gmail.com>
2020-05-23 13:11:23 +0200

    anado servlet a mi fichero sabadete

--- sabado.java
+++ sabado.java
@@ -19,3 +19,48 @@ public String getPayloadObjeto() throws JsonProcessingException {
 
     return payloadJson;
 }
+
+<Invoice>
+  <Header invoiceNumber="12345">
+    <Date>
+      <Month>July</Month>
+      <Day>15</Day>
+      <Year>2001</Year>
+    </Date>
+    <BillTo custNumber="X5739" name="Milton McGoo" phone="416-448-4414">
+      <Address>
+        <street1>IBM</street1>
+        <street2>1150 Eglinton Ave East</street2>
+        <city>Toronto</city>
+        <state>Ontario</state>
+        <zip>M3C 1H7</zip>
+        <country>Canada</country>
+      </Address>s
+    </BillTo>
+  </Header>
+  <Item discount="promotion" price="57">
+    <description>high speed 3D graphics card</description>
+  </Item>
+</Invoice>
+package com.mavenproyecto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html; charset=UTF-8"); // 
+        resp.getWriter().print("<html>\n" +  // 
+                "<body>\n" +
+                "<h1>This is my HTML page</h1>\n" +
+                "<p> hallo what is going on!!!</p>\n" +
+                "</body>\n" +
+                "</html>");
+    }
+}
